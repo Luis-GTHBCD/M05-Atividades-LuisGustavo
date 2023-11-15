57de72c8f3dd57e2101024e1ca376dc597daae0a
Luis-GTHBCD <145384000+Luis-GTHBCD@users.noreply.github.com>
2023-11-15 11:00:55 -0300

    questão1.js

---
+++ Semana4-JS/atividade1/questão1.js
@@ -0,0 +1,13 @@
+class Animal {
+    constructor(name){
+        this.name = name;
+    }
+}
+
+class aligator extends Animal{
+    constructor(name, strongjaw){
+        super(name);
+        this.strongjaw = strongjaw;
+
+    }
+}
